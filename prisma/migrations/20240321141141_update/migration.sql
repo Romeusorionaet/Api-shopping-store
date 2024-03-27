c4f9efbe16a8a5892bb721f9ad4e15ec1b91b27e
romeusorionaet <romeusoares14569@gmail.com>
2024-03-27 10:47:48 -0300

    schema updated

--- prisma/migrations/20240321141141_update/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "buyer_address" DROP CONSTRAINT "buyer_address_orderId_fkey";
-
--- AddForeignKey
-ALTER TABLE "buyer_address" ADD CONSTRAINT "buyer_address_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20240324181523_create/migration.sql
+++ prisma/migrations/20240324181523_create/migration.sql
@@ -5,7 +5,39 @@ CREATE TYPE "ModeOfSale" AS ENUM ('SELLS_ONLY_IN_THE_REGION', 'ONLINE_STORE');
 CREATE TYPE "OrderStatus" AS ENUM ('WAITING_FOR_PAYMENT', 'PAYMENT_CONFIRMED');
 
 -- CreateEnum
-CREATE TYPE "AddressRole" AS ENUM ('ORDER', 'BUYER');
+CREATE TYPE "OrderStatusTracking" AS ENUM ('WAITING', 'CANCELED', 'PRODUCT_DELIVERED_TO_CARRIER', 'PRODUCT_DELIVERED_TO_CLIENT');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_address" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "cep" INTEGER NOT NULL,
+    "city" TEXT NOT NULL,
+    "uf" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "house_number" INTEGER NOT NULL,
+    "complement" TEXT NOT NULL,
+    "phone_number" INTEGER NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_address_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "categories" (
@@ -35,10 +67,10 @@ CREATE TABLE "products" (
     "width" INTEGER NOT NULL,
     "height" INTEGER NOT NULL,
     "weight" INTEGER NOT NULL,
-    "placeOfSale" "ModeOfSale" NOT NULL DEFAULT 'ONLINE_STORE',
     "stars" INTEGER DEFAULT 0,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "placeOfSale" "ModeOfSale" NOT NULL DEFAULT 'ONLINE_STORE',
     "categoryId" TEXT NOT NULL,
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
@@ -47,38 +79,14 @@ CREATE TABLE "products" (
 -- CreateTable
 CREATE TABLE "orders" (
     "id" TEXT NOT NULL,
-    "status" "OrderStatus" NOT NULL DEFAULT 'WAITING_FOR_PAYMENT',
-    "productId" TEXT NOT NULL,
-    "buyer_address" TEXT NOT NULL,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "tracking_code" TEXT NOT NULL DEFAULT '',
+    "order_status_tracking" "OrderStatusTracking" NOT NULL DEFAULT 'WAITING',
+    "status" "OrderStatus" NOT NULL DEFAULT 'WAITING_FOR_PAYMENT',
     "buyerId" TEXT NOT NULL,
 
     CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "order_products" (
-    "id" TEXT NOT NULL,
-    "base_price" DECIMAL(8,2) NOT NULL,
-    "discount_percentage" INTEGER NOT NULL DEFAULT 0,
-    "quantity" INTEGER NOT NULL,
-    "productId" TEXT NOT NULL,
-    "orderId" TEXT NOT NULL,
-
-    CONSTRAINT "order_products_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "users" (
-    "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password_hash" TEXT NOT NULL,
-    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -96,20 +104,32 @@ CREATE TABLE "buyer_address" (
     "email" TEXT NOT NULL,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "role" "AddressRole" NOT NULL DEFAULT 'BUYER',
     "buyerId" TEXT NOT NULL,
+    "orderId" TEXT,
 
     CONSTRAINT "buyer_address_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "order_products" (
+    "id" TEXT NOT NULL,
+    "base_price" DECIMAL(8,2) NOT NULL,
+    "discount_percentage" INTEGER NOT NULL DEFAULT 0,
+    "quantity" INTEGER NOT NULL,
+    "productId" TEXT NOT NULL,
+    "orderId" TEXT,
+
+    CONSTRAINT "order_products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "categories_title_key" ON "categories"("title");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "products_title_key" ON "products"("title");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "products" ADD CONSTRAINT "products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -118,10 +138,13 @@ ALTER TABLE "products" ADD CONSTRAINT "products_categoryId_fkey" FOREIGN KEY ("c
 ALTER TABLE "orders" ADD CONSTRAINT "orders_buyerId_fkey" FOREIGN KEY ("buyerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "buyer_address" ADD CONSTRAINT "buyer_address_buyerId_fkey" FOREIGN KEY ("buyerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "buyer_address" ADD CONSTRAINT "buyer_address_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "order_products" ADD CONSTRAINT "order_products_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "order_products" ADD CONSTRAINT "order_products_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "buyer_address" ADD CONSTRAINT "buyer_address_buyerId_fkey" FOREIGN KEY ("buyerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "order_products" ADD CONSTRAINT "order_products_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
