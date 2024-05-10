9f88c774de120f0c288d0264c8304020b84f9dab
romeusorionaet <romeusoares14569@gmail.com>
2024-05-10 14:42:45 -0300

    remove refresh token

--- src/infra/http/controllers/user/auth/routes.ts
+++ src/infra/http/controllers/user/auth/routes.ts
@@ -3,10 +3,12 @@ import { registerWithGoogle } from "./register-with-google";
 import { register } from "./register";
 import { refreshToken } from "./refresh-token";
 import { authenticate } from "./authenticate";
+import { removeRefreshToken } from "./remove-refresh-token";
 
 export async function authRoutes(app: FastifyInstance) {
   app.get("/auth/register/oauth-google/callback", registerWithGoogle);
   app.post("/auth/user/register", register);
   app.post("/auth/user/refresh-token", refreshToken);
   app.post("/auth/user/authenticate", authenticate);
+  app.post("/auth/user/signUp", removeRefreshToken);
 }
