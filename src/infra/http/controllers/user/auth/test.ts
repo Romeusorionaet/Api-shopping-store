43a059545d411c190fe006ee003ed8d885460efc
romeusorionaet <romeusoares14569@gmail.com>
2024-05-16 10:01:48 -0300

    remove domain name

--- src/infra/http/controllers/user/auth/test.ts
+++ src/infra/http/controllers/user/auth/test.ts
@@ -2,21 +2,14 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { env } from "src/infra/env";
 
 export async function testRoute(request: FastifyRequest, reply: FastifyReply) {
-  console.log(
-    "==test==env",
-    env.DOMAIN_COOKIE_TOKEN,
-    env.SHOPPING_STORE_URL_WEB,
-  );
+  console.log("==test==env", env.SHOPPING_STORE_URL_WEB);
 
   return reply
-    .status(200)
     .setCookie("@shopping-store/AT.2.0", "at", {
       httpOnly: true,
       secure: true,
       sameSite: "none",
       path: "/",
-      domain: env.DOMAIN_COOKIE_TOKEN,
     })
-    .redirect(env.SHOPPING_STORE_URL_WEB)
-    .status(200);
+    .redirect(env.SHOPPING_STORE_URL_WEB);
 }
