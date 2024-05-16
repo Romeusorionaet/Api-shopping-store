a9dde40d9b69a522aa847c8178596a30833f19de
romeusorionaet <romeusoares14569@gmail.com>
2024-05-16 09:53:03 -0300

    show log env for test

--- src/infra/http/controllers/user/auth/test.ts
+++ src/infra/http/controllers/user/auth/test.ts
@@ -2,15 +2,21 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { env } from "src/infra/env";
 
 export async function testRoute(request: FastifyRequest, reply: FastifyReply) {
-  console.log("==teste");
+  console.log(
+    "==test==env",
+    env.DOMAIN_COOKIE_TOKEN,
+    env.SHOPPING_STORE_URL_WEB,
+  );
 
   return reply
+    .status(200)
     .setCookie("@shopping-store/AT.2.0", "at", {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       path: "/",
       domain: env.DOMAIN_COOKIE_TOKEN,
     })
-    .redirect(env.SHOPPING_STORE_URL_WEB);
+    .redirect(env.SHOPPING_STORE_URL_WEB)
+    .status(200);
 }
