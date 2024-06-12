6d670d8c63d82ac8a74f865855827ce3e215a497
romeusorionaet <romeusoares14569@gmail.com>
2024-06-12 14:51:40 -0300

    created test for register with google

--- src/infra/http/controllers/user/auth/register-with-google.e2e-spec.ts
+++ src/infra/http/controllers/user/auth/register-with-google.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { app } from "src/app";
+import request from "supertest";
 
 describe("Register With Google (E2E)", () => {
   beforeAll(async () => {
@@ -9,5 +10,19 @@ describe("Register With Google (E2E)", () => {
     await app.close();
   });
 
-  test("[POST] /auth/register/oauth-google", async () => {});
+  test("[POST] /auth/register/oauth-google/callback", async () => {
+    const response = await request(app.server)
+      .post("/auth/register/oauth-google/callback")
+      .send({
+        username: "User test 01",
+        email: "usertest01@gmail.com",
+      });
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        accessToken: expect.any(String),
+        refreshToken: expect.any(String),
+      }),
+    );
+  });
 });
