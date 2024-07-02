5846ab189ec750231a987fd7a14c5734220538c6
romeusorionaet <romeusoares14569@gmail.com>
2024-07-02 19:55:32 -0300

    added "RUST_BACKTRACE: 1" for more details and applied config from dotenv in prisma setup

--- src/infra/service/setup-prisma/prisma.ts
+++ src/infra/service/setup-prisma/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import { config } from "dotenv";
 import { env } from "src/infra/env";
+
+config();
+console.log("Node env:", env.NODE_ENV);
 
 export const prisma = new PrismaClient({
   log: env.NODE_ENV === "dev" ? ["query"] : [],
