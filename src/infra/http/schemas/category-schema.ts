16963688dcdfbc5bc09bf9ada581c145bf7099ed
Romeu Sorionaet <romeusoares14569@gmail.com>
2024-11-11 12:27:01 -0300

    refactor: divide schema for different case

--- src/infra/http/schemas/category-schema.ts
+++ src/infra/http/schemas/category-schema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const categoryCreateSchema = z.object({
   title: z.string().min(1, { message: "Título obrigatório." }),
   imgUrl: z.string().min(1, { message: "Url da imagem obrigatório." }),
+  commit: z.string().min(1, { message: "Descrição da operação obrigatório." }),
 });
 
 export const categoryUpdateSchema = z.object({
--- src/infra/http/schemas/sub-admin-schema.ts
+++ src/infra/http/schemas/sub-admin-schema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const subSchema = z.object({
+export const subAdminSchema = z.object({
   sub: z.string().uuid("Não autorizado"),
   publicId: z.string().uuid("Não autorizado"),
+  staffId: z.string().uuid("Não autorizado"),
+  role: z.string(),
 });
