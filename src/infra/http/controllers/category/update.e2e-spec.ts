ec1ec2e5fd47622ded7b19d39572ce3bb54a53a9
Romeu Sorionaet <romeusoares14569@gmail.com>
2024-11-13 15:25:10 -0300

    test: passed token in request

--- src/infra/http/controllers/category/update.e2e-spec.ts
+++ src/infra/http/controllers/category/update.e2e-spec.ts
@@ -2,14 +2,18 @@ import request from "supertest";
 import { app } from "src/infra/app";
 import { prisma } from "src/infra/service/setup-prisma/prisma";
 import { CategoryFactory } from "test/factories/make-category";
+import { CreateAndAuthenticateUserAdminWithTokensFactory } from "test/factories/make-create-and-authenticate-user";
 
 describe("Update Category (E2E)", () => {
   let categoryFactory: CategoryFactory;
+  let createAndAuthenticateUserAdminWithTokensFactory: CreateAndAuthenticateUserAdminWithTokensFactory;
 
   beforeAll(async () => {
     await app.ready();
 
     categoryFactory = new CategoryFactory();
+    createAndAuthenticateUserAdminWithTokensFactory =
+      new CreateAndAuthenticateUserAdminWithTokensFactory();
   });
 
   afterAll(async () => {
@@ -17,15 +21,23 @@ describe("Update Category (E2E)", () => {
   });
 
   test("[POST] /category/update", async () => {
+    const { accessToken } =
+      await createAndAuthenticateUserAdminWithTokensFactory.makePrismaCreateAndAuthenticateUserAdminWithTokens(
+        app,
+      );
+
     const category = await categoryFactory.makePrismaCategory({
       title: "category test title create 01",
     });
 
-    const response = await request(app.server).put("/category/update").send({
-      id: category.id.toString(),
-      title: "category test 01 updated",
-      imgUrl: category.imgUrl,
-    });
+    const response = await request(app.server)
+      .put("/category/update")
+      .send({
+        id: category.id.toString(),
+        title: "category test 01 updated",
+        imgUrl: category.imgUrl,
+      })
+      .set("Authorization", `Bearer ${accessToken}`);
 
     expect(response.statusCode).toEqual(201);
 
